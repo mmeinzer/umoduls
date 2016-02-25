f5103054b25b45ce66b049f4a790c70ad4e797ef
mmeinzer <meinzer.matthew@gmail.com>
2016-02-25 12:31:20 -0500

    adds main site app 'webapp'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'webapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
